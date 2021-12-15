44c416f6087e93c35dc9d2adcf289adfbd8d7a69
JotaVe <jotavexd@gmail.com>
2021-12-14 22:42:37 -0300

    Fix_Testing

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const client = new Client({
   restTimeOffset: 0
 });
 
-client.login(TOKEN);
+client.login(process.env.BOT_TOKEN);
 client.commands = new Collection();
 client.prefix = PREFIX;
 client.queue = new Map();
